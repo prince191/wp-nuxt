c25da3e89bfad6b071822d622f0907ef968fd9a0
prince191 <vuongvinh.mdc@gmail.com>
2024-03-20 23:03:11 +0700

    vinhvh

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,6 @@ const baseUrl = "https://gaucoffee.com/"
 const fullUrl = "https://admin.gaucoffee.com/"
 
 export default {
-  target: 'static',  
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     htmlAttrs: {
