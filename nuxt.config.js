d13d812d6e0528d052b00c433bebcb95532680f0
prince191 <vuongvinh.mdc@gmail.com>
2024-03-21 15:01:47 +0700

    vinhvh

--- nuxt.config.js
+++ nuxt.config.js
@@ -73,5 +73,10 @@ export default {
   env: {
     baseUrl: baseUrl,
     graphUrl: fullUrl + 'graphql',
+  },
+  server: {
+    port: 3000, // default: 3000
+    host: '0.0.0.0',
+    timing: false
   }
 }
