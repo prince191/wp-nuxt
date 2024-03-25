62bcda53ddc5852e4e82da5f2732b20ccd6a9074
Vương Hồng Vĩnh <vuongvinh.mdc@gmail.com>
2024-03-25 11:17:21 +0700

    vinhvh

--- nuxt.config.js
+++ nuxt.config.js
@@ -75,7 +75,7 @@ export default {
     graphUrl: fullUrl + 'graphql',
   },
   server: {
-    port: 3000, // default: 3000
+    port: 8088, // default: 3000
     host: '0.0.0.0',
     timing: false
   }
